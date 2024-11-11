0ba01c8d4e52f58ad928f22a146cb4345e497247
ozerbaykal <baykalozer87@gmail.com>
2024-11-11 05:58:55 +0300

    showTypingAnimation

--- main.js
+++ main.js
@@ -2,11 +2,12 @@
 const chatInput = document.querySelector("#chat-input")
 const sendButton = document.querySelector("#send-btn");
 const defaultText = document.querySelector(".default-text");
+const chatContainer = document.querySelector(".chat-container");
 
 
 let userText = null
 
-
+//gönderdiğimiz html class isimine göre ibze bir html oluşturur.
 const createELement = (html, className) => {
     //yeni div oluşutur
     const chatDiv = document.createElement("div")
@@ -15,12 +16,36 @@ const createELement = (html, className) => {
 
     //oluşturduğumuz divin içerisine dışarıdan gelen html parametreisni ekle i ekle
     chatDiv.innerHTML = html
-    console.log(chatDiv)
+
 
 
 
     return chatDiv
 }
+
+const showTypingAnimation = () => {
+    const html = `
+   <div class="chat-content">
+        <div class="chat-details">
+            <img src="./images/chatbot.jpg" alt="" />
+            <div class="typing-animation">
+                <div class="typing-dot" style="--delay: 0.2s"></div>
+                <div class="typing-dot" style="--delay: 0.3s"></div>
+                <div class="typing-dot" style="--delay: 0.4s"></div>
+            </div>
+        </div>
+   </div>
+    
+    `;
+
+    const incomingChatDiv = createELement(html, "incoming")
+    chatContainer.appendChild(incomingChatDiv)
+
+
+}
+
+
+
 
 const handleOutGoingChat = () => {
 
@@ -43,9 +68,17 @@ const handleOutGoingChat = () => {
     
     
     `;
+    //kullanıcının mesajını içeren bğr div oluştur ve chatContainer yapısına ekle
     const outgoingChatDiv = createELement(html, "outgoing")
 
     defaultText.remove()
+
+    outgoingChatDiv.querySelector("p").textContent = userText
+    chatContainer.appendChild(outgoingChatDiv)
+
+    setTimeout(showTypingAnimation, 500)
+
+        ;
 
 }
 
