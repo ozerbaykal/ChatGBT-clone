e8eb68fa5b691c854da13c3aa24fbaad2d86cffd
ozerbaykal <baykalozer87@gmail.com>
2024-11-11 04:41:02 +0300

    createElement

--- main.js
+++ main.js
@@ -1,13 +1,51 @@
 // Html
 const chatInput = document.querySelector("#chat-input")
 const sendButton = document.querySelector("#send-btn");
+const defaultText = document.querySelector(".default-text");
+
 
 let userText = null
 
-const handleOutGoingChat = (e) => {
-    e.preventDefault()
-    userText = chatInput.value
 
+const createELement = (html, className) => {
+    //yeni div oluşutur
+    const chatDiv = document.createElement("div")
+    //oluşturduğumuz dive parametre olrak aldığığımız className i class olarak ekle
+    chatDiv.classList.add("chat", className)
+
+    //oluşturduğumuz divin içerisine dışarıdan gelen html parametreisni ekle i ekle
+    chatDiv.innerHTML = html
+    console.log(chatDiv)
+
+
+
+    return chatDiv
+}
+
+const handleOutGoingChat = () => {
+
+    userText = chatInput.value.trim();
+    //inputun içinde veri yoksa durdur
+    if (!userText) {
+        alert("Bir veri giriniz")
+        return;
+    }
+
+    const html = `
+    <div class="chat-content">
+          <div class="chat-details">
+            <img src="./images/user.jpg" alt="" />
+            <p></p>
+          </div>
+    </div>
+    
+    
+    
+    
+    `;
+    const outgoingChatDiv = createELement(html, "outgoing")
+
+    defaultText.remove()
 
 }
 
